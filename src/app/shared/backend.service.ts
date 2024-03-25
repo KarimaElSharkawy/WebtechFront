8de080b61f5eb39fb48ec5a30941d12d4ba3192f
KarimaElSharkawy <92715986+KarimaElSharkawy@users.noreply.github.com>
2024-03-25 23:03:56 +0100

    deploy

---
+++ src/app/shared/backend.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Eintraege } from './eintraege';
+import { environment } from '../../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendService {
+  backendUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getAllEntries(): Observable<Eintraege[]> {
+    return this.http.get<Eintraege[]>(`${this.backendUrl}/entries`);
+  }
+
+  createOneEntry(eintrag: Eintraege): Observable<Eintraege> {
+    return this.http.post<Eintraege>(`${this.backendUrl}/addEntry`, eintrag);
+  }
+
+  deleteEntry(id: number): Observable<any> {
+    return this.http.delete(`${this.backendUrl}/entries/${id}`);
+  }
+
+  createUserProfile(profile: any): Observable<any> {
+    return this.http.post(`${this.backendUrl}/users`, profile);
+  }
+  
+  getUserProfile(): Observable<any> {
+    return this.http.get(`${this.backendUrl}/users`);
+  }
+  clearUserProfile(): Observable<any> {
+    return this.http.delete(`${this.backendUrl}/users`);
+}
+updateEntry(id: number, entry: Eintraege): Observable<Eintraege> {
+  return this.http.put<Eintraege>(`${this.backendUrl}/entries/${id}`, entry);
+}
+}
